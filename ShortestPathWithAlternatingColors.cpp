f972d9b09cc3cbe80b069c9db46072e104609032
10luo <55992221+10luo@users.noreply.github.com>
2020-07-16 00:02:52 -0400

    Add files via upload

---
+++ ShortestPathWithAlternatingColors.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> shortestAlternatingPaths (int n, vector<vector<int>> red_edges, vector<vector<int>> blue_edges) 
+{
+    vector<bool> red(n,0);
+    vector<bool> blue(n,0);
+    vector<int> result(n,-1);
+    vector<vector<int>> current;
+    vector<vector<vector<int>>> graph(n);
+
+    int steps = 0;
+    result[0] = 0;
+    
+    for (vector<int> i : red_edges){
+        graph[i[0]].push_back({i[1],1});
+    }
+    for (vector<int> i : blue_edges){
+        graph[i[0]].push_back({i[1],0});
+    }
+
+    for (vector<int> i: graph[0]){
+        current.push_back(i);
+        if (i[1]) red[i[0]] = 1;
+        else blue[i[0]] = 1;
+    }
+    
+    
+    while (!current.empty()){
+        steps++;
+        vector<vector<int>> next;
+        for (vector<int> i:current){
+            int node = i[0];
+            int col = i[1];
+            if (result[node] == -1) result[node] = steps;
+            for (vector<int> j: graph[node]){
+                if (col){
+                    if (!blue[j[0]] && !j[1]){
+                        next.push_back(j);
+                        blue[j[0]] = 1;
+                    }
+                }
+                else{
+                    if (!red[j[0]] && j[1]){
+                        next.push_back(j);
+                        red[j[0]] = 1;
+                    }
+                }
+            }                
+        }
+        current = next;     
+    }   
+return result;
+}
+int main() {
+    int n=5;
+    vector<vector<int>> red_edges = {{0,1},{1,2},{2,3},{3,4}};
+    vector<vector<int>> blue_edges = {{1,2},{2,3},{3,1}};
+    vector<int> res = shortestAlternatingPaths(n,red_edges,blue_edges);
+    for (int i: res) cout<<i<<' ';
+    return 0;
+};
