be902ba4e643ae44b7db2aaa62106ed8baf1ed31
10luo <55992221+10luo@users.noreply.github.com>
2020-07-16 20:45:15 -0400

    Add files via upload

---
+++ Pow_xn.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+double myPow(double x, int n) {
+        if (n==0) return 1;
+        double temp = myPow(x,n/2);
+        if (n%2)
+            return n>0?temp*temp*x:temp*temp/x;
+        else 
+            return temp*temp;      
+}
+int main(){
+    double x = 10.00;
+    int n = 20;
+    cout<<222<<endl;
+    cout<<myPow(x,n)<<endl;
+
+    return 0;
+}
